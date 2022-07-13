a4a73099c05dedd8368ef8773172a27dec09e237
VolodymyrNehir <volodymyrnehir@gmail.com>
2022-07-14 00:39:21 +0300

    add

--- index.js
+++ index.js
@@ -1,15 +1,46 @@
 const telegramBotApi = require('node-telegram-bot-api');
 const url = require("url");
+const {debug} = require("nodemon/lib/utils");
 
-const token = '5391660001:AAGQd7SgJECb3GdPary7QzAu7e-5TkjcTWg'
+const token = '5391660001:AAGQd7SgJECb3GdPary7QzAu7e-5TkjcTWg';
 
 const bot = new telegramBotApi(token,{polling:true})
+
+bot.setMyCommands([
+    {command:'/go', description:'підкинути монетку'}
+])
+
+
+
 bot.on('message',msg =>{
+   const text = msg.text
    const chatID = msg.chat.id;
    const randomNumber = Math.floor(Math.random()*10)
-    if (randomNumber > 5){
-        bot.sendPhoto(chatID, './img/telegram-cloud-photo-size-2-5361854923882479983-x.jpg')
-    } else {
+
+
+
+   const options =
+    {
+        reply_markup: JSON.stringify({
+            inline_keyboard: [
+                [{
+                    text: 'google',
+                    callback_data: '/go'
+                }]
+
+            ]
+        })
+    }
+
+
+    bot.on('callback_query', query =>{
+        const go = query.data.search('/go')
+       return  bot.sendMessage(query.message.chat.id, 'ik',options)
+    })
+
+     if (text === '/go' & (randomNumber % 2 === 0) ){
+        bot.sendSticker(chatID, 'CAACAgIAAxkBAAEFQthizsQ84J6jCPbF7yWjwgSPebi0lAACUx0AAtfzaUqygSLXQdkB4ikE')
+    } if (text === '/go' & (randomNumber % 2 === 1) ) {
         bot.sendSticker(chatID,'./img/img.png')
     }
 
