4b7755fb214a6d42f34f25077126685d0005063d
VolodymyrNehir <volodymyrnehir@gmail.com>
2022-07-19 00:57:09 +0300

    add

--- index.js
+++ index.js
@@ -4,43 +4,44 @@ const bot = new telegramBotApi(token, {polling: true})
 
 const button = {
     reply_markup: JSON.stringify({
-        inline_keyboard:[
-            [{text:'go', callback_data:'2'}]
+        inline_keyboard: [
+            [{text: 'go', callback_data: '1'}]
         ]
 
     })
 }
-
-bot.setMyCommands([
-    {command: '/go', description: 'підкинути монетку'}
-])
-// Command
+// button
 
 bot.on('message', msg => {
     const text = msg.text
     const chatID = msg.chat.id;
-    const randomNumber = Math.floor(Math.random() * 10)
-    // Random
+    // message
 
     const go = async () => {
-        if (text === '/go' & (randomNumber % 2 === 0) || text === '/start' & (randomNumber % 2 === 0)) {
+       const randomNumber = Math.floor(Math.random() * 10)
+        // Random
+
+        if (chatID & (randomNumber % 2 === 0) || text === '/start' & (randomNumber % 2 === 0)) {
             await bot.sendSticker(chatID, 'CAACAgIAAxkBAAEFQthizsQ84J6jCPbF7yWjwgSPebi0lAACUx0AAtfzaUqygSLXQdkB4ikE')
-            await bot.sendMessage(chatID, '/go'
+            await bot.sendMessage(chatID, 'Решка', button,
             )
         }
-        if (text === '/go' & (randomNumber % 2 === 1) || text === '/start' & (randomNumber % 2 === 1)) {
+        if (chatID & (randomNumber % 2 === 1) || text === '/start' & (randomNumber % 2 === 1)) {
             await bot.sendSticker(chatID, 'CAACAgIAAxkBAAEFR6hi0JO8UVRs6oG_8_lFrM7IAAEnpuYAApYUAAIk34hK6VCO6-8kkIApBA')
-            await bot.sendMessage(chatID, '/go', button
+            await bot.sendMessage(chatID, 'Орел', button
             )
         }
     }
 
-    bot.on('callback_data', data=>{
-        console.log(data)
-        // if (data.message.chat.id === data.message.chat.id){
-// bot.sendMessage(data.message.chat.id,data)
 
+    bot.on('callback_query', async data => {
+        if (data.message.chat.id) {
+            go()
+        }
     })
-    go()
 
+    // action
+if (text === '/start'){
+   return  go()
+}
 })
