5a03f62edb0885ffd23f80061ded47a43b76fe8a
VolodymyrNehir <volodymyrnehir@gmail.com>
2022-07-20 23:22:49 +0300

    add

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const telegramBotApi = require('node-telegram-bot-api');
+const {quiet} = require("nodemon/lib/utils");
 const token = '5391660001:AAGQd7SgJECb3GdPary7QzAu7e-5TkjcTWg';
 const bot = new telegramBotApi(token, {polling: true})
+const exspress = require('express');
+
+const app = exspress()
 
 var port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
@@ -48,3 +52,6 @@ if (text === '/start'){
    return  go()
 }
 })
+app.listen(1997, () => {
+    console.log('PORT  1997');
+});
